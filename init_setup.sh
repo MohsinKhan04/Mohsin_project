7bb7ff356f87aa85e050a56f70bf54255bd8c3a0
Rattlesnake <mk8086560@gmail.com>
2025-10-27 20:13:21 +0530

    updated

---
+++ init_setup.sh
@@ -0,0 +1,19 @@
+
+echo [$(date)]: "START"
+
+
+echo [$(date)]: "creating env with python version" 
+
+
+conda create --prefix ./env python  -y
+
+
+echo [$(date)]: "activating the environment" 
+
+source activate ./env
+
+echo [$(date)]: "installing the dev requirements" 
+
+pip install -r requirements.txt
+
+echo [$(date)]: "END" 
